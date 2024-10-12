6648ef6230cbea5a28f7b82094ee5e1545b11039
Edward <91560320+unique-EJ@users.noreply.github.com>
2024-10-12 17:21:51 +0100

    Update gadget-setup.sh - bind mount operation to attach other mount point
    mount configfs as type configfs to /config or from /sys/kernel/config (bind operation to attach original mount point)

--- gadget-setup.sh
+++ gadget-setup.sh
@@ -20,7 +20,8 @@ UDC=ci_hdrc.0                                                     #  name of the
 
 # gadget configuration
 modprobe libcomposite                                             #  load configfs
-mount none /config -t configfs                                    #  mount configfs as type configfs at /config
+mount -t configfs none /config || { \                             #  mount configfs as type configfs to /config or from /sys/kernel/config (bind operation)
+  mkdir /config && mount --rbind -t configfs /sys/kernel/config /config
 mkdir /config/usb_gadget/keyboardgadget                           #  make a new gadget skeleton
 cd /config/usb_gadget/keyboardgadget                              #  cd to gadget dir
 mkdir configs/c.1                                                 #  make the skeleton for a config for this gadget
